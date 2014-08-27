4c1e2e531811b4561e6acc109150c740a2ecae23
Corentin Delcourt <codl@codl.fr>
2014-08-27 21:19:37 +0200

    dennis

---
+++ dennis.js
@@ -0,0 +1,15 @@
+$(function($){
+    var we_have_dennis = false;
+    function dennis(){
+        if(we_have_dennis) return;
+
+        we_have_dennis = true;
+
+        var audio = $('<audio/>', { src: '/dennis.ogg', loop: true })[0];
+        audio.style.display = 'none';
+        $("pre").after(audio);
+        audio.play();
+        $("html").addClass("dennis");
+    }
+    $('#dennis').on('click', dennis);
+});
