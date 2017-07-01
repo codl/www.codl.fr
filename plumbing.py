941da314920ce43e286de7a8e96e9a765d0e5382
codl <codl@codl.fr>
2017-07-01 18:00:42 +0200

    add cachebusting

--- plumbing.py
+++ plumbing.py
@@ -3,7 +3,8 @@ legacy redirects and hacky routing
 """
 
 from codl import app
-from flask import redirect, url_for
+from flask import redirect, url_for, abort
+import os
 
 @app.route('/herd.html')
 def redir_herd():
@@ -14,3 +15,28 @@ def redir_herd():
 @app.route('/humans.txt', defaults={'filename': 'humans.txt'})
 def route_to_static(filename):
     return app.view_functions['static'](filename=filename)
+
+@app.route('/static-<int:timestamp>/<path:filename>')
+def static_cachebust(timestamp, filename):
+    path = os.path.join(app.static_folder, filename)
+    mtime = os.stat(path).st_mtime
+    if abs(mtime - timestamp) > 1:
+        abort(404)
+    else:
+        resp = route_to_static(filename)
+        resp.headers.set('cache-control', 'public, immutable, max-age=%s' % (60*60*24*365,))
+        if 'expires' in resp.headers:
+            resp.headers.remove('expires')
+        return resp
+
+@app.context_processor
+def replace_url_for():
+    return dict(url_for = cachebust_url_for)
+
+def cachebust_url_for(endpoint, **kwargs):
+    if endpoint == 'static':
+        endpoint = 'static_cachebust'
+        path = os.path.join(app.static_folder, kwargs.get('filename'))
+        kwargs['timestamp'] = int(os.stat(path).st_mtime)
+    return url_for(endpoint, **kwargs)
+
