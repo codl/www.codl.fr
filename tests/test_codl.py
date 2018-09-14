883772b14e09270331c94729ce34f3fd5487ac00
codl <codl@codl.fr>
2018-09-14 09:14:50 +0200

    fix regexes (regices?) in tests

--- tests/test_codl.py
+++ tests/test_codl.py
@@ -6,7 +6,7 @@ from flask import url_for
 def strip_html(html):
     soup = BeautifulSoup(html, "html.parser")
     content = " ".join(soup.stripped_strings)
-    content = re.sub('[\s]+', ' ', content)
+    content = re.sub(r'[\s]+', ' ', content)
     return content
 
 
@@ -18,5 +18,5 @@ def test_index(client):
 def test_contact(client):
     resp = client.get(url_for('contact'))
     soup = BeautifulSoup(resp.get_data(as_text=True), 'html.parser')
-    assert soup.find('a', href=re.compile('^mailto:'))
-    assert soup.find('a', href=re.compile('^https://chitter\.xyz'))
+    assert soup.find('a', href=re.compile(r'^mailto:'))
+    assert soup.find('a', href=re.compile(r'^https://chitter\.xyz'))
