04e67897d1094bc75e583fb19592351d110f2688
codl <codl@codl.fr>
2019-05-22 02:59:04 +0200

    fix tests

--- tests/test_codl.py
+++ tests/test_codl.py
@@ -4,7 +4,8 @@ from flask import url_for
 
 
 def test_contact(client):
-    resp = client.get(url_for('contact'))
+    resp = client.get(url_for('contact'), follow_redirects=True)
     soup = BeautifulSoup(resp.get_data(as_text=True), 'html.parser')
     assert soup.find('a', href=re.compile(r'^mailto:')) is not None
-    assert soup.find('a', href=re.compile(r'^https://chitter\.xyz'), rel='me') is not None
+    assert soup.find('a', href=re.compile(r'^https://chitter\.xyz'),
+                     rel='me') is not None
