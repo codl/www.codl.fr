08bf6bc8d946159a7daab5af42333a940d1dc338
codl <codl@codl.fr>
2022-03-21 03:16:54 +0100

    mini gallery stuff. this doesn't deploy right yet

--- codl/codl.py
+++ codl/codl.py
@@ -3,15 +3,75 @@ Main pages go here
 """
 from flask import Flask, render_template, make_response
 import importlib
+from mastodon import Mastodon
+import redis
+import pickle
+from typing import Optional
+import requests
 
 app = Flask(__name__)
+app.config.from_pyfile("config.py")
 importlib.import_module("codl.plumbing")
+
+
+class NoMastodonAccess(Exception):
+    pass
+
+
+redis_conn: Optional[redis.Redis] = None
+
+
+def get_redis() -> redis.Redis:
+    """Instanciate redis client or return existing"""
+    global redis_conn
+    if not redis_conn:
+        host = app.config.get("REDIS_HOST", "127.0.0.1")
+        port = app.config.get("REDIS_PORT", "6379")
+        db = app.config.get("REDIS_DB", "0")
+        redis_conn = redis.Redis(host=host, port=port, db=db)
+
+    return redis_conn
+
+
+def recent_artworks() -> list[tuple[str, str]]:
+    """Return a list of (post_url, thumbnail_url) tuples for recent public media posts on donphan"""
+    CACHE_KEY = "www.codl.fr:1:artworks"
+    r = get_redis()
+    cached = r.get(CACHE_KEY)
+    if not cached:
+        access_token = app.config.get("MASTODON_ACCESS_TOKEN")
+        if not access_token:
+            raise NoMastodonAccess()
+
+        session = requests.Session()
+        session.headers.update({"user-agent": "www.codl.fr"})
+        m = Mastodon(
+            access_token=access_token,
+            api_base_url="https://donphan.social/",
+            session=session,
+        )
+
+        me = m.me()
+        statuses = m.account_statuses(me["id"], only_media=True, exclude_replies=True)
+        artworks = list()
+        for status in statuses:
+            if not status["sensitive"] and status["visibility"] == "public":
+                artworks.append(
+                    (status["url"], status["media_attachments"][0]["preview_url"])
+                )
+
+        r.set(CACHE_KEY, pickle.dumps(artworks), ex=3600)
+
+    else:
+        artworks = pickle.loads(cached)
+
+    return artworks
 
 
 @app.route("/index.html")
 @app.route("/")
 def index():
     "Serves the index page."
-    resp = make_response(render_template("index.html"))
+    resp = make_response(render_template("index.html", artworks=recent_artworks()))
     resp.add_etag()
     return resp
